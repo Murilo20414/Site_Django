3e62291fb389ec0d0e567c20af2ae9f6d839245f
Murilo <murilos@itmchannerlmarketing.com>
2021-04-10 21:36:23 -0300

    .

--- contact/forms.py
+++ contact/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label='Nome: ', max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder': 'Digite seu nome'}))
+    name = forms.CharField(label='Nome: ', max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder': 'Digite seu nome para contato'}))
     email = forms.EmailField(label='E-mail:', required=True, widget=forms.TextInput(attrs={'placeholder': 'Digite seu e-mail'}))
     message = forms.CharField(label='Mensagem:', required=True, widget=forms.Textarea(attrs={'placeholder': 'Escreva para nós...'}),)
