86091e5762e7e633bac4bc6ea0d6927dc23ac78a
Murilo <murilos@itmchannerlmarketing.com>
2021-04-10 22:54:13 -0300

    .

--- meu_site/settings.py
+++ meu_site/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*','https://project-blogy.herokuapp.com/']
+ALLOWED_HOSTS = ['*','https://project-blogy.herokuapp.com']
 
 
 # Application definition
